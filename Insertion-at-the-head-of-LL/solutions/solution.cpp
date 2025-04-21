e06097359105b98f12b6e507148426cf59b2604a
Shleshitha Cheekati <153906990+shleshitha@users.noreply.github.com>
2025-04-21 17:57:38 +0530

    Update solution.cpp

--- Insertion-at-the-head-of-LL/solutions/solution.cpp
+++ Insertion-at-the-head-of-LL/solutions/solution.cpp
@@ -1,32 +1,12 @@
-#include <stack>
-#include <unordered_map>
-#include <string>  // Required for std::string
-using namespace std; 
- 
-// ListNode.h
-#ifndef LISTNODE_H
-#define LISTNODE_H
-
 struct ListNode {
     int data;
     ListNode* next;
-
-    ListNode(int val) {
-        data = val;
-        next = nullptr;
-    }
+    ListNode(int val) : data(val), next(nullptr) {}
 };
 
-#endif
-
-
 class Solution {
-    public:
-        ListNode* insertAtHead(ListNode* &head, int X) {
-            ListNode* newNode = new ListNode(X);
-            newNode->next = head;
-            head = newNode;  
-            return head; 
-        }
-    };
-    
+public:
+    ListNode* insertAtHead(ListNode*& head, int X) {
+        return NULL;
+    }
+};
