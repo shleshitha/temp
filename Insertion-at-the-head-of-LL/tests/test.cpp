14eca062e63ea22a097898a03d933d47b4740cf8
Shleshitha Cheekati <153906990+shleshitha@users.noreply.github.com>
2025-04-21 17:57:15 +0530

    Update test.cpp

--- Insertion-at-the-head-of-LL/tests/test.cpp
+++ Insertion-at-the-head-of-LL/tests/test.cpp
@@ -1,25 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <climits>
+#include "../solutions/solution.cpp"  // This file already has ListNode and Solution
+
 using namespace std;
-
-// ListNode definition
-struct ListNode {
-    int data;
-    ListNode* next;
-    ListNode(int val) : data(val), next(nullptr) {}
-};
-
-// Solution class
-class Solution {
-public:
-    ListNode* insertAtHead(ListNode*& head, int X) {
-        ListNode* newNode = new ListNode(X);
-        newNode->next = head;
-        head = newNode;
-        return head;
-    }
-};
 
 // Utility to create a linked list from vector
 ListNode* createList(vector<int>& arr) {
@@ -61,20 +45,20 @@ void runTestCase(vector<int> inputList, int X, vector<int> expectedList, int* pa
     }
 }
 
-// Main function with 10 test cases
+// Main function
 int main() {
     int passed = 0, total = 0;
 
-    runTestCase({1, 2, 3}, 0, {0, 1, 2, 3}, &passed, &total);           // Insert at head, normal list
-    runTestCase({}, 10, {10}, &passed, &total);                        // Insert in empty list
-    runTestCase({5}, 7, {7, 5}, &passed, &total);                      // Insert at head, single node
-    runTestCase({-1, -2, -3}, -4, {-4, -1, -2, -3}, &passed, &total);  // Negative values
-    runTestCase({100, 200}, 300, {300, 100, 200}, &passed, &total);    // Larger integers
-    runTestCase({1, 1, 1}, 1, {1, 1, 1, 1}, &passed, &total);          // All elements the same
-    runTestCase({9, 8, 7}, 10, {10, 9, 8, 7}, &passed, &total);        // Increasing head insert
-    runTestCase({0}, 0, {0, 0}, &passed, &total);                      // Insert 0 at head
-    runTestCase({INT_MAX}, INT_MIN, {INT_MIN, INT_MAX}, &passed, &total); // Edge int values
-    runTestCase({2, 4, 6, 8}, 1, {1, 2, 4, 6, 8}, &passed, &total);    // General even list
+    runTestCase({1, 2, 3}, 0, {0, 1, 2, 3}, &passed, &total);
+    runTestCase({}, 10, {10}, &passed, &total);
+    runTestCase({5}, 7, {7, 5}, &passed, &total);
+    runTestCase({-1, -2, -3}, -4, {-4, -1, -2, -3}, &passed, &total);
+    runTestCase({100, 200}, 300, {300, 100, 200}, &passed, &total);
+    runTestCase({1, 1, 1}, 1, {1, 1, 1, 1}, &passed, &total);
+    runTestCase({9, 8, 7}, 10, {10, 9, 8, 7}, &passed, &total);
+    runTestCase({0}, 0, {0, 0}, &passed, &total);
+    runTestCase({INT_MAX}, INT_MIN, {INT_MIN, INT_MAX}, &passed, &total);
+    runTestCase({2, 4, 6, 8}, 1, {1, 2, 4, 6, 8}, &passed, &total);
 
     cout << passed << "/" << total << " test cases passed." << endl;
     return 0;
